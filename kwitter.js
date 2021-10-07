1a2d6b4b0830ae1c241e60ea6397ce8cc812f0aa
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2021-10-07 20:22:30 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,12 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAS0Z-hyAka4UUEYvP7oqrFy0k22AmqwTY",
-    authDomain: "chit-chat-45715.firebaseapp.com",
-    databaseURL: "https://chit-chat-45715-default-rtdb.firebaseio.com",
-    projectId: "chit-chat-45715",
-    storageBucket: "chit-chat-45715.appspot.com",
-    messagingSenderId: "988060589267",
-    appId: "1:988060589267:web:87f8f42a99ff8f10480826"
-  };
 
 
 
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,90 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAS0Z-hyAka4UUEYvP7oqrFy0k22AmqwTY",
+    authDomain: "chit-chat-45715.firebaseapp.com",
+    databaseURL: "https://chit-chat-45715-default-rtdb.firebaseio.com",
+    projectId: "chit-chat-45715",
+    storageBucket: "chit-chat-45715.appspot.com",
+    messagingSenderId: "988060589267",
+    appId: "1:988060589267:web:87f8f42a99ff8f10480826"
+  };
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+  
+
+
+    name_welcome=localStorage.getItem("user_name");
+document.getElementById("welcome").innerHTML="WELCOME!"+"  "+ name_welcome +" "+ ":)";
+
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+row="<div class='room_name1' id="+Room_names+"onclick='redirectToRoom(this.id)'>#"+Room_names+"</div><hr>";
+       document.getElementById("output").innerHTML += row;
+//End code
+});});}
+getData();
+
+
+function redirectToRoom(rooms){
+    console.log("selected_room="+ rooms);
+    localStorage.setItem("room_name", rooms);
+
+    window.location="kwitter_page.html";
+}
+function addRoom(){
+    room=document.getElementById("room_name").value;
+    localStorage.setItem("room_name" ,room);
+    firebase.database().ref("/").child(room).update({purpose:"add room name"});
+
+    window.location ="kwitter_room.html";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function logOut(){
     window.location="index.html";
 }
+
+
+ 
+
