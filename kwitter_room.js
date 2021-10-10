58345c82416207fe76e0d45bf3c8224c986a18d7
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2021-10-10 20:34:50 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,19 +1,8 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAS0Z-hyAka4UUEYvP7oqrFy0k22AmqwTY",
-    authDomain: "chit-chat-45715.firebaseapp.com",
-    databaseURL: "https://chit-chat-45715-default-rtdb.firebaseio.com",
-    projectId: "chit-chat-45715",
-    storageBucket: "chit-chat-45715.appspot.com",
-    messagingSenderId: "988060589267",
-    appId: "1:988060589267:web:87f8f42a99ff8f10480826"
-  };
-  // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+
   
 
-
-    name_welcome=localStorage.getItem("user_name");
-document.getElementById("welcome").innerHTML="WELCOME!"+"  "+ name_welcome +" "+ ":)";
+username=localStorage.getItem("user_name");
+document.getElementById("welcome").innerHTML="WELCOME!"+"  "+ username +" "+ ":)";
 
 
 function getData() {firebase.database().ref("/").on('value',
